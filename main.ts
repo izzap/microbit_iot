bebec374bf18578f85d86ca51f47fbcb30e2622e
Daniel Juhl Christoffersen <72377@grundfos.com>
2020-02-04 20:00:11 +0100

    updated

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ namespace ESP8266Azure {
     let wifi_connected: boolean = false
     let azure_connected: boolean = false
     let last_upload_successful: boolean = false
+    let serial_str: string = ""
 
     // write AT command with CR+LF ending
     function sendAT(command: string, wait: number = 100) {
@@ -15,20 +16,19 @@ namespace ESP8266Azure {
     }
 
     // wait for certain response from ESP8266
-    function waitResponse(): boolean {
-        let serial_str: string = ""
+    function wait_for_response(str: string): boolean {
         let result: boolean = false
         let time: number = input.runningTime()
         while (true) {
             serial_str += serial.readString()
-            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
-            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+            if (serial_str.length > 200) {
+                serial_str = serial_str.substr(serial_str.length - 200)
+            }
+            if (serial_str.includes(str)) {
                 result = true
                 break
-            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
-                break
             }
-            if (input.runningTime() - time > 30000) break
+            if (input.runningTime() - time > 300000) break
         }
         return result
     }
@@ -37,45 +37,33 @@ namespace ESP8266Azure {
     * Initialize ESP8266 module and connect it to Wifi router
     */
     //% block="Initialize ESP8266|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
-    //% tx.defl=SerialPin.P0
-    //% rx.defl=SerialPin.P1
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P12
     //% ssid.defl=your_ssid
     //% pw.defl=your_pw
     export function connectWifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pw: string) {
         wifi_connected = false
         azure_connected = false
-        serial.redirect(
-            tx,
-            rx,
-            baudrate
-        )
+        serial.redirect(tx, rx, baudrate)
+
         sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+RST", 1000)
         sendAT("AT+CWMODE=1") // set to STA mode
-        sendAT("AT+RST", 1000) // reset
+        
         sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
-        wifi_connected = waitResponse()
-        basic.pause(100)
-    }
 
-    /**
-     * Read response
-     */
-    //% block="Read response"
-    export function readResponse(): string {
-        let response: string = ""
-        let time: number = input.runningTime()
-        while (true) {
-            response += serial.readString()
-            if (response.length > 10) {
-                break;
-            }
+        wifi_connected = wait_for_response("OK")
 
-            if (input.runningTime() - time > 30000) {
-                break
-            }
-        }
-        return response
+        if (!wifi_connected) 
+            control.reset()
+
+        // display IP (you'll need this in STA mode; in AP mode it would be default 192.168.4.1)
+        sendAT("AT+CIFSR")
+        
+        // startup completed
+        basic.showIcon(IconNames.Yes)
     }
+z
    
     /**
      * Get Workday
@@ -83,18 +71,29 @@ namespace ESP8266Azure {
     //% block="Get workday "
     export function GetWorkDay() : string {
         if (wifi_connected) {
-            sendAT("AT+CIPSTART=\"TCP\",\"http://workassistantapi.azurewebsites.net\",80", 0) // connect to website server
-
-            basic.pause(100)
-           
-            let str: string = "GET /api/workday"
-            sendAT("AT+CIPSEND=" + str.length +2)
-            sendAT(str, 0)
-
+            sendAT('AT+CIPSTART="TCP","104.41.216.137",80', 0) // connect to website server
+            azure_connected = waitResponse("OK")
             basic.pause(100)
 
-            return "Done sending"
+            if (azure_connected) {
+                let str: string = "GET /api/workday"
+                sendAT("AT+CIPSEND=" + str.length +2)
+                sendAT(str, 0)
 
+                last_upload_successful = waitResponse("SEND OK")
+                basic.pause(100)
+
+                if(last_upload_successful) {
+                    sendAT(AT+CIPCLOSE)
+
+                    if(wait_for_response("CLOSED\nOK")) {
+                        azure_connected = false;
+                    }
+                }
+
+                return "success http"
+            }
+            return "no server"
         }
 
         return "no wifi"

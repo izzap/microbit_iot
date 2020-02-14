75f5906e7fedaeac218ba6cd20a32d7942cfa576
Daniel Juhl Christoffersen <72377@grundfos.com>
2020-02-14 12:59:33 +0100

    now a combination

--- main.ts
+++ main.ts
@@ -1,15 +1,49 @@
+enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    DELETE,
+    PATCH,
+    OPTIONS,
+    CONNECT,
+    TRACE
+}
+
+enum Newline {
+    CRLF,
+    LF,
+    CR
+}
+
+
 /**
  * MakeCode extension for Elecfreak IOT ESP8266 Wifi modules and Azure IOT
  */
-//% color=#11497B icon="\uf1eb" block="ESP8266 Azure"
-namespace ESP8266Azure {
+//% color=#11497B icon="\uf1eb" block="PIT"
+namespace PIT {
 
     let wifi_connected: boolean = false
-    let azure_connected: boolean = false
+    let api_connected: boolean = false
     let last_upload_successful: boolean = false
+    let userToken_def: string = ""
+    let topic_def: string = ""
+
+    const EVENT_ON_ID = 100
+    const EVENT_ON_Value = 200
+    const EVENT_OFF_ID = 110
+    const EVENT_OFF_Value = 210
+    let toSendStr = ""
+
+    export enum State {
+        //% block="Success"
+        Success,
+        //% block="Fail"
+        Fail
+    }
 
     // write AT command with CR+LF ending
-    function sendAT(command: string, wait: number = 100) {
+    function sendAT(command: string, wait: number = 0) {
         serial.writeString(command + "\u000D\u000A")
         basic.pause(wait)
     }
@@ -21,111 +55,101 @@ namespace ESP8266Azure {
         let time: number = input.runningTime()
         while (true) {
             serial_str += serial.readString()
-            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.length > 200)
+                serial_str = serial_str.substr(serial_str.length - 200)
             if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
                 result = true
                 break
-            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
+            }
+            if (serial_str.includes("ERROR") || serial_str.includes("FAIL")) {
                 break
             }
-            if (input.runningTime() - time > 30000) break
+            if (input.runningTime() - time > 5000) {
+                break
+            }
         }
         return result
     }
 
-    // wait for certain response from ESP8266
-    function getResponse(): string {
-        let serial_str: string = ""
-        let time: number = input.runningTime()
-        while (true) {
-            serial_str += serial.readString()
-            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
-            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
-                break
-            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
-                break
-            }
-            if (input.runningTime() - time > 1000) {
-                serial_str = "timeout"
-                break
-            }
-        }
-        return serial_str
-    }
-
     /**
-    * Initialize ESP8266 module and connect it to Wifi router
+    * Initialize ESP8266 module 
     */
-    //% block="Initialize ESP8266|RX (Tx of micro:bit) %tx|TX (Rx of micro:bit) %rx|Baud rate %baudrate|Wifi SSID = %ssid|Wifi PW = %pw"
-    //% tx.defl=SerialPin.P0
-    //% rx.defl=SerialPin.P1
+    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P12
     //% ssid.defl=your_ssid
-    //% pw.defl=your_pw
-    export function connectWifi(tx: SerialPin, rx: SerialPin, baudrate: BaudRate, ssid: string, pw: string) {
-        wifi_connected = false
-        azure_connected = false
+    //% pw.defl=your_password
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
         serial.redirect(
             tx,
             rx,
             baudrate
         )
         sendAT("AT+RESTORE", 1000) // restore to factory settings
-        sendAT("AT+RST", 1000) // reset
         sendAT("AT+CWMODE=1") // set to STA mode
-
-        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
-        wifi_connected = waitResponse()
-        sendAT("AT + CIPDOMAIN=\"8.8.8.8\"", 100)
+        sendAT("AT+RST", 1000) // reset
         basic.pause(100)
     }
-
     /**
-     * Read response
-     */
-    //% block="Read response"
-    export function readResponse(): string {
-        let response: string = ""
-        let time: number = input.runningTime()
-        while (true) {
-            response += serial.readString()
-            if (response.length > 10) {
-                break;
-            }
+    * connect to Wifi router
+    */
+    //% block="connect Wifi SSID = %ssid|KEY = %pw"
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw
+    export function connectWifi(ssid: string, pw: string) {
 
-            if (input.runningTime() - time > 30000) {
-                break
-            }
-        }
-        return response
+        wifi_connected = false
+        api_connected = false
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        wifi_connected = waitResponse()
+        basic.pause(100)
     }
-
+    
     /**
-     * Get Workday
+     * Execute HTTP method.
+     * @param method HTTP method, eg: HttpMethod.GET
+     * @param host Host, eg: "google.com"
+     * @param port Port, eg: 80
+     * @param urlPath Path, eg: "/search?q=something"
+     * @param headers Headers
+     * @param body Body
      */
-    //% block="Get workday "
-    export function GetWorkDay(): string {
-        let response: string = "no wifi"
-
-        if (wifi_connected) {
-            sendAT("AT+CIPSTART=\"TCP\",\"http://workassistantapi.azurewebsites.net\",80", 0) // connect to website server
-
-            response = getResponse()
-            basic.pause(100)
-
-            sendAT("AT+CWLIF", 100)
-            response = response + "\n" + getResponse()
-
-
-            let str: string = "GET /api/workday"
-            sendAT("AT+CIPSEND=" + str.length + 2)
-            sendAT(str, 0)
-
-            response = response + "\n" + getResponse()
-            basic.pause(100)
-
+    //% weight=96
+    //% blockId="wfb_http" block="execute HTTP method %method|host: %host|port: %port|path: %urlPath||headers: %headers|body: %body"
+    export function executeHttpMethod(method: HttpMethod, host: string, port: number, urlPath: string, headers?: string, body?: string): void {
+        let myMethod: string
+        switch (method) {
+            case HttpMethod.GET: myMethod = "GET"; break;
+            case HttpMethod.POST: myMethod = "POST"; break;
+            case HttpMethod.PUT: myMethod = "PUT"; break;
+            case HttpMethod.HEAD: myMethod = "HEAD"; break;
+            case HttpMethod.DELETE: myMethod = "DELETE"; break;
+            case HttpMethod.PATCH: myMethod = "PATCH"; break;
+            case HttpMethod.OPTIONS: myMethod = "OPTIONS"; break;
+            case HttpMethod.CONNECT: myMethod = "CONNECT"; break;
+            case HttpMethod.TRACE: myMethod = "TRACE";
         }
+        // Establish TCP connection:
+        let data: string = "AT+CIPSTART=\"TCP\",\"" + host + "\"," + port
+        sendAT(data, pauseBaseValue * 6)
 
-        return response
+        api_connected = waitResponse()
+        basic.pause(100)
+
+        data = myMethod + " " + urlPath + " HTTP/1.1" + "\u000D" + "\u000A"
+            + "Host: " + host + "\u000D" + "\u000A"
+        if (headers && headers.length > 0) {
+            data += headers + "\u000D" + "\u000A"
+        }
+        if (data && data.length > 0) {
+            data += "\u000D" + "\u000A" + body + "\u000D" + "\u000A"
+        }
+        data += "\u000D" + "\u000A"
+        // Send data:
+        sendAT("AT+CIPSEND=" + (data.length + 2), pauseBaseValue * 3)
+        sendAT(data, pauseBaseValue * 6)
+        // Close TCP connection:
+        sendAT("AT+CIPCLOSE", pauseBaseValue * 3)
     }
 
     /**
@@ -140,25 +164,28 @@ namespace ESP8266Azure {
     /**
     * Check if ESP8266 successfully connected to Wifi
     */
-    //% block="Wifi connected ?"
-    export function isWifiConnected() {
-        return wifi_connected
+    //% block="Wifi connected %State"
+    export function wifiState(state: boolean) {
+        if (wifi_connected == state) {
+            return true
+        }
+        else {
+            return false
+        }
     }
 
+    
     /**
-    * Check if ESP8266 successfully connected to Azure
+    * Check if ESP8266 successfully uploaded data to ThingSpeak
     */
-    //% block="Azure connected ?"
-    export function isAzureConnected() {
-        return azure_connected
+    //% block="ThingSpeak Last data upload %State"
+    //% subcategory="ThingSpeak"
+    export function tsLastUploadState(state: boolean) {
+        if (last_upload_successful == state) {
+            return true
+        }
+        else {
+            return false
+        }
     }
-
-    /**
-    * Check if ESP8266 successfully uploaded data to Azure
-    */
-    //% block="Last data upload successful ?"
-    export function isLastUploadSuccessful() {
-        return last_upload_successful
-    }
-
 }
